ddf0af2cd481cd79f800d4085db725cfc1064378
EmilKamel <Emil.kloster.lindberg@gmail.com>
2016-04-12 09:29:34 +0200

    Noget af entity

---
+++ Project Gruppe 6/DBLayer/EntityFrameworkContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLayer
+{
+    public class EntityFrameworkContext : DbContext
+    {
+        EntityFrameworkContext()
+        {
+
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        //public DbSet<CLASS> NAMES { get; set; }
+
+    }
+}
