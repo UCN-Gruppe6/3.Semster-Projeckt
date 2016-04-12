2423620baea0f35c2fdde16c04a954d0bb743e4e
Martin Brander Fonnesberg <1037841@ucn.dk>
2016-04-12 10:08:38 +0200

    Basket

---
+++ Project Gruppe 6/ModelLayer/Basket/Basket.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Basket
+{
+    public class BasketL
+    {
+        CPU MyCpu = new CPU();
+        GPU MyGpu = new GPU();
+        Motherboard MyMotherboard = new Motherboard();
+        Socket MySocket = new Socket();
+        RAM MyRam = new RAM();
+        CPUCooler MyCpuCooler = new CPUCooler();
+        Carbinet MyCarbinet = new Carbinet();
+        Storiage MyStoriage = new Storiage();
+    }
+
+    public void BasketList()
+    {
+        Console.WriteLine(BasketL);
+        Console.ReadKey();
+    }
+}
