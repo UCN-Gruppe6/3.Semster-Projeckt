28163f856ee2a261a25a1ea5b78ed8bb0acb712b
EmilKamel <Emil.kloster.lindberg@gmail.com>
2016-04-12 10:21:54 +0200

    Fixed basket

--- Project Gruppe 6/ModelLayer/Basket/Basket.cs
+++ Project Gruppe 6/ModelLayer/Basket/Basket.cs
@@ -5,23 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModelLayer.Basket
-{
-    public class BasketL
-    {
-        CPU MyCpu = new CPU();
-        GPU MyGpu = new GPU();
-        Motherboard MyMotherboard = new Motherboard();
-        Socket MySocket = new Socket();
-        RAM MyRam = new RAM();
-        CPUCooler MyCpuCooler = new CPUCooler();
-        Carbinet MyCarbinet = new Carbinet();
-        Storiage MyStoriage = new Storiage();
-    }
+//namespace ModelLayer.Basket
+//{
+    //public class BasketL
+    //{
+        //CPU MyCpu = new CPU();
+        //GPU MyGpu = new GPU();
+        //Motherboard MyMotherboard = new Motherboard();
+        //Socket MySocket = new Socket();
+        //RAM MyRam = new RAM();
+        //CPUCooler MyCpuCooler = new CPUCooler();
+        //Carbinet MyCarbinet = new Carbinet();
+        //Storiage MyStoriage = new Storiage();
+  //  }
 
-    public void BasketList()
-    {
-        Console.WriteLine(BasketL);
-        Console.ReadKey();
-    }
-}
+    //public void BasketList()
+    //{
+     //   Console.WriteLine("BasketL");
+     //   Console.ReadKey();
+    //}
+//}
