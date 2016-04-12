7d6167881126c20bd973a04d3e8c75b6631b2a1d
EmilKamel <Emil.kloster.lindberg@gmail.com>
2016-04-12 10:17:10 +0200

    added test project + sample test

---
+++ Project Gruppe 6/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int beginning = 10;
+            int creditAmount = 10;
+            int expected = 20;
+            int x = 0;
+
+            x = beginning + creditAmount;
+
+            Assert.AreEqual(expected, x, 0);
+        }
+    }
+}
