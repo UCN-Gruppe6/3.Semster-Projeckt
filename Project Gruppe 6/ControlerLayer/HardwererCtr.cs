04db9eacfe6323863a4b37c26eb06b6c9da82cbe
Mads <tinsfeldt88@gmail.com>
2016-04-15 09:09:06 +0200

    Ikke noget vigtigt

---
+++ Project Gruppe 6/ControlerLayer/HardwererCtr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using DBLayer;
+using ModelLayer.Hardware;
+
+namespace ControlerLayer
+{
+    public class HardwererCtr
+    {
+        // CPU START
+
+        public void CreateCPU(CPU cpu)
+        {
+            using (EntityFrameworkContext db = new EntityFrameworkContext())
+            {
+                db.CPUs.Add(cpu);
+                db.SaveChanges();
+            }
+        }
+
+        public CPU GetCPUByBrand(string Brand)
+        {
+            using (EntityFrameworkContext db = new EntityFrameworkContext())
+            {
+
+            }
+        }
+
+        public IEnumerable<CPU> GetAllCPUs()
+        {
+            using (EntityFrameworkContext db = new EntityFrameworkContext())
+            {
+                return db.CPUs.ToList();
+            }
+        }
+    }
+}
