ed0e7af8f1738c8016cbd1ebdea85f344ecd0999
Mads <tinsfeldt88@gmail.com>
2016-04-19 10:56:35 +0200

    WCF

---
+++ Project Gruppe 6/WCFServices/IPcPartServise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer.Hardware;
+using System.ServiceModel;
+
+namespace WCFServices
+{
+    [ServiceContract]
+    interface IPcPartServise
+    {
+        // Hardware
+        #region
+
+        // CPU START
+
+        [OperationContract]
+        IEnumerable<CPU> FindCPUsByBrand(string Brand);
+
+        [OperationContract]
+        IEnumerable<CPU> FindCPUsByCategory(string Category);
+
+        [OperationContract]
+        IEnumerable<CPU> FindAllCPUs();
+
+        // CPU END
+
+        #endregion
+
+    }
+}
---
+++ Project Gruppe 6/WCFServices/PcPartServise.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using ModelLayer.Hardware;
+using ControlerLayer;
+
+namespace WCFServices 
+{
+    public class PcPartServise : IPcPartServise
+    {
+
+        private HardwererCtr HWCtr = new HardwererCtr();
+
+        // Hardware
+        #region
+
+        // CPU START
+
+        public IEnumerable<CPU> FindAllCPUs()
+        {
+            return HWCtr.FindAllCPUs();
+        }
+
+        public IEnumerable<CPU> FindCPUsByBrand(string Brand)
+        {
+            return HWCtr.FindCPUsByBrand(Brand);
+        }
+
+        public IEnumerable<CPU> FindCPUsByCategory(string Category)
+        {
+            return HWCtr.FindCPUsByCategory(Category);
+        }
+
+        // CPU END
+
+        #endregion
+    }
+}
