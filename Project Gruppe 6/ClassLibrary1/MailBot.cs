0599b900b398e82427acb8e18fb11c9c1a100cee
EmilKamel <Emil.kloster.lindberg@gmail.com>
2016-05-24 09:38:22 +0200

    MailClient

---
+++ Project Gruppe 6/ClassLibrary1/MailBot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using ModelLayer.Basket;
+using ModelLayer.Hardware;
+using System.Net;
+using System.Net.Mail;
+
+namespace Mailbot
+{
+    public class MailBot
+    {
+
+        public void SendInvoiceMail(Basket basket, String ToAddress )
+        {
+            try {
+                const string FromAddress = "KlostRobot@gmail.com";
+                const string FromPassword = "RobotRobot1";
+                string subject = "Order nr: ";
+                string body = "Test";
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("Smtp.gmail.com");
+                mail.From = new MailAddress(FromAddress);
+                mail.To.Add(ToAddress);
+                mail.Subject = subject;
+                mail.Body = body;
+                SmtpServer.Port = 465;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(FromAddress, FromPassword);
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+
+            }
+            catch(Exception e)
+            {
+                
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Project Gruppe 6/UnitTestProject/MailTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLayer;
+using Mailbot;
+using ModelLayer.Basket;
+using ModelLayer.Hardware;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class MailTest
+    {
+        [TestMethod]
+        public void SendTestMailTilEmil()
+        {
+            if(true)
+            {
+                Basket basket = new Basket();
+                MailBot mailbot = new MailBot();
+
+                mailbot.SendInvoiceMail(basket, "Emil.kloster.lindberg@gmail.com");
+            }
+
+
+
+            Assert.AreEqual(1, 1, "yo");
+        }
+    }
+}
